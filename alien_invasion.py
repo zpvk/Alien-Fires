53413c179597857d8fd7c454aad731a93b61057c
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2021-03-11 02:11:07 +0530

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,3 +20,12 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Make the most recently drawn screen visible
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
