daeb7865e47129435d4aded5cb4ce231821beb25
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2021-03-22 23:41:38 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,4 +2,8 @@ class Settings:
     """A class to store all setting for alien Invasion"""
 
     def __init__(self):
-        """In"""
+        """Initialize the game's settings."""
+        #screen setting
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
