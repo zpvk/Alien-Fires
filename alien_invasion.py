11237f9d878bf7979385775919c30076a094e9ae
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2021-03-22 23:44:25 +0530

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 
+from settings import Settings
 
 class AlienInvasion:
     """ Overall class to manage game assets and behavior. """
@@ -9,6 +10,10 @@ class AlienInvasion:
     def __init__(self):
         """ Initialize the game, and create game resources """
         pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height)
+        )
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
